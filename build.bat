0307bf857c5f9b8b6aab1b5facb93e7ae9672d83
DESKTOP-AECREV8\98DU <just_merlini@126.com>
2024-05-30 21:47:02 +0800

    作业

---
+++ build.bat
@@ -0,0 +1,3 @@
+gcc first.c -c -o first.o
+gcc first.o libraylibdll.a -o first.exe
+first.exe
---
+++ first.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "include/raylib.h"
+
+int main()
+{
+
+    //初始化窗口
+    InitWindow(1200,800,"哈哈哈");
+    float x = 600;
+    int y = 400;
+    float speed = 100;
+    
+    
+    
+    while (!WindowShouldClose())
+    {
+        
+        float m_delta = GetFrameTime();    
+        x = x + m_delta * speed;
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawCircle(x,y,44,BLUE);
+        EndDrawing();
+    }
+    
+    
+
+    return 0;
+
+}
