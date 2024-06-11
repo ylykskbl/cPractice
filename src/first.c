5caddf645990a6b85ecc2e462295a6955afcb575
ylykskbl <just_merlini@126.com>
2024-06-11 19:01:02 +0800

    数组边界问题
    测试代码

--- src/first.c
+++ src/first.c
@@ -4,12 +4,14 @@
 
 void Init(PjtBall *p_ball)
 {
-
+    
     //初始坐标
     p_ball->posX = 600;
     p_ball->posY = 400;
     p_ball->speedBase = 200;    //小球速度
     //初始化窗口
+    
+
     InitWindow(1200,800,"哈哈哈");
 }
 
@@ -23,13 +25,12 @@ void Imput_changeColor(PjtBall *p_ball)
     {
         rndi_color = GetRandomValue(0,6);
         p_ball->color = *(p_color + rndi_color);
-        printf("%u %u %u %u\n",p_ball->color.r,p_ball->color.g,p_ball->color.b,p_ball->color.a);
+        //printf("%u %u %u %u\n",p_ball->color.r,p_ball->color.g,p_ball->color.b,p_ball->color.a);
     };
     
-
 }
 
-void Dologic(PjtBall *p_ball)
+void Dologic(PjtBall *p_ball,PjtBall p_balls[],int *p_ballNum)
 {
     float m_delta = GetFrameTime();
     
@@ -40,7 +41,6 @@ void Dologic(PjtBall *p_ball)
     {
         p_ball->speedUp = 0;
     };
-
     if(IsKeyDown(KEY_S))
     {
         p_ball->speedDown = 1;
@@ -74,14 +74,40 @@ void Dologic(PjtBall *p_ball)
 
     //按键输入时随机变化颜色
     Imput_changeColor(p_ball);
+    //鼠标点击时在数组中创建小球
+    //CreateBallByClick(p_balls[0],*p_ballNum);
+    if (IsMouseButtonPressed(0))
+    {
+        Vector2 m_pos = GetMousePosition();
+        p_balls[*p_ballNum].posX = m_pos.x;
+        p_balls[*p_ballNum].posY = m_pos.y;
+        p_balls[*p_ballNum].color = RED;
+        *p_ballNum = *p_ballNum + 1;
+        //printf("%u\n",*p_ballNum);
+    }
+
+    //鼠标右键移除小球
+}
+
+void CreateBallByClick(PjtBall p_balls[],int *p_ballNum)
+{
+        
     
 }
 
-void Performance(PjtBall *p_ball)
+void Performance(PjtBall *p_ball,PjtBall p_balls[],int *p_ballNum)
 {
     BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawCircle(p_ball->posX,p_ball->posY,44,p_ball->color);
+        //画出所有小球
+        //printf("%u\n",*p_ballNum);
+        for (int i = 0; i < *p_ballNum; i++)
+        {
+            DrawCircle(p_balls[i].posX,p_balls[i].posY,30,p_balls[i].color);
+        }
+        
+
     EndDrawing();
 }
 
@@ -91,6 +117,9 @@ int main()
     PjtBall m_ball;
     //设置默认颜色
     m_ball.color = RED;
+
+    PjtBall m_balls[1];    //一个小球组
+    int m_ballNum = 0;  //小球数量
     
     
     //初始化
@@ -99,8 +128,8 @@ int main()
     //主循环
     while (!WindowShouldClose())
     {
-        Dologic(&m_ball);
-        Performance(&m_ball);
+        Dologic(&m_ball,m_balls,&m_ballNum);
+        Performance(&m_ball,m_balls,&m_ballNum);
         
     }
     
